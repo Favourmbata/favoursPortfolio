58d882d8786910a745172b0ff3b3568c00479e8f
Favourmbata <134517996+Favourmbata@users.noreply.github.com>
2024-09-19 08:47:18 +0100

    updated my portfolio

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -2,7 +2,9 @@ import styles from "./Projects.module.css"
 import ChurchLogo from  "../../assert/church-logo.png"
 import ProjectCard from "../../reuseable/projectCard/ProjectCard";
 import Kidjig from  "../../assert/Rectangle 2@2x.png"
-
+import hobank from "../../assert/hoobank.svg"
+import  Gpt3 from "../../assert/logo.svg"
+import  Gericht from "../../assert/gericht.png"
 const Projects = ()=>{
     return(
         <section id="projects" className={styles.container}>
@@ -18,8 +20,29 @@ const Projects = ()=>{
                 <ProjectCard
                     src={Kidjig}
                     link="https://kisjig.vercel.app/"
-                    h3=""
-                    />
+                    h3="Kidjid landing page"
+                />
+            </div>
+            <div>
+                <ProjectCard
+                    src={hobank}
+                    link="https://modern-baking-webpage.vercel.app/"
+                    h3="modern banking webpage"
+                />
+            </div>
+            <div>
+                <ProjectCard
+                    src={Gericht}
+                    link="https://restaurant-app-five-chi.vercel.app/"
+                    h3="Active resturant website"
+                />
+            </div>
+            <div>
+                <ProjectCard
+                    src={Gpt3}
+                    link="https://gpt-3-rouge-three.vercel.app/"
+                    h3="Gpt3 version landing page"
+                />
             </div>
         </section>
     )
